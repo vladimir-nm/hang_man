797c0218647e42ea5acc07d706de1e39262d07d9
Vladimir Mordakin <mordakin.vn@yandex.ru>
2021-05-25 23:10:53 +0300

    migration events users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  VALID_EMAIL = /\A\w+@\w+\.[a-z]+\z/
+
+  has_many :events
+
+  validates :name,
+            presence: true,
+            length: { maximum: 35 }
+
+  validates :email,
+            uniqueness: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL }
 end
